5066f3cda3c87a6be8c983eee885acf1898afbea
fixdq <fixd.quan@gmail.com>
2021-10-28 17:48:44 +0800

    update config

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# 常用插件
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# passwall 依赖
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
